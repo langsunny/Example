de90940971b2d1689b0e16417e3b86079f506f28
langsunny <langxl@hfut.edu.cn>
2019-03-18 03:28:57 +0800

    添加项目文件。

---
+++ unit1/AlohaWorld/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//using namespace std;
+using std::cout;  //Recommended
+using std::endl;
+using std::cin;
+
+int main() {  //This is main funtion
+	cout << "Aloha, World!" << endl;  //end line
+	// keep output windows
+	cin.get();
+	return 0;
+}
